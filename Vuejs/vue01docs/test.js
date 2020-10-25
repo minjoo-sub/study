29fb8252693ebc85bdc1b21d5291a2b6f14ff69e
minjoo <minjooo9696@naver.com>
2020-10-25 18:10:13 +0900

    2020-10-25

---
+++ Vuejs/vue01docs/test.js
@@ -0,0 +1,70 @@
+//import axios from 
+;
+
+new Vue({
+    /* edtmc^2w */
+    el: "",
+    data: {
+
+    },
+    //template: ``,
+    methods: {
+
+    },
+    components: {},
+    computed: {
+
+    },
+    watch: {
+
+    },
+    // 라이프 사이클과 상관 있는 것들
+    mounted: function () {
+        console.log("mounted");
+    },
+    updated: function () {
+        console.log("updated");
+    },
+});
+
+Vue.component("test", {
+    /* pdtmc^2w */
+    props: [],
+    data: function () {
+        return {}
+    },
+    template: `
+
+    `,
+    methods: {
+
+    },
+    //components: { },
+    computed: {
+
+    },
+    watch: {
+
+    },
+});
+
+Vue.component("test", {
+    /* pdtmc^2w */
+    props: [],
+    data: function () {
+        return {}
+    },
+    template: `
+
+    `,
+    methods: {
+
+    },
+    components: {},
+    computed: {
+
+    },
+    watch: {
+
+    },
+});

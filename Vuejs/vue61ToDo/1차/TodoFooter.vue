5e404494ac95692547bb7fb34583ebe5c5535afa
minjoo <minjooo9696@naver.com>
2020-11-15 18:12:05 +0900

    2020-11-15

--- Vuejs/vue61ToDo/1차/TodoFooter.vue
+++ Vuejs/vue61ToDo/1차/TodoFooter.vue
@@ -1,20 +1,20 @@
 <style scoped>
 .clearAllContainer {
-    width: 8.5rem;
-    height: 50px;
-    line-height: 50px;
-    background-color: white;
-    border-radius: 5px;
-    margin: 0 auto;
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  background-color: white;
+  border-radius: 5px;
+  margin: 0 auto;
 }
 .clearAllBtn {
-    color: #e20303;
-    display: block;
+  color: #e20303;
+  display: block;
 }
 </style>
 
 <template>
-    <div class="clearAllContainer">
-        <span class="clearAllBtn" v-on:click="clearTodo">Clear All</span>
-    </div>
+  <div class="clearAllContainer">
+    <span class="clearAllBtn" v-on:click="clearAll">Clear All</span>
+  </div>
 </template>
--- Vuejs/vue61ToDo/2차/Todo.vue
+++ Vuejs/vue61ToDo/2차/Todo.vue
@@ -1,27 +1,38 @@
 <style scoped>
 body {
-    text-align: center;
-    background-color: #f6f6f8;
+  text-align: center;
+  background-color: #f6f6f8;
 }
 input {
-    border-style: groove;
-    width: 200px;
+  border-style: groove;
+  width: 200px;
 }
 button {
-    border-style: groove;
+  border-style: groove;
 }
 .shadow {
-    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
 }
 </style>
 
 <template>
-    <div id="app">
-        <TodoHeader></TodoHeader>
-        <TodoInput v-on:addTodo="addTodo"></TodoInput>
-        <TodoList v-bind:propsdata="todoItems" v-on:removeTodo="removeTodo"></TodoList>
-        <TodoFooter v-on:removeAll="clearAll"></TodoFooter>
-    </div>
+  <div id="app">
+    <!-- TodoHeader -->
+    <TodoHeader></TodoHeader>
+
+    <!-- TodoInput -->
+    <TodoInput v-on:addTodo="addTodo"></TodoInput>
+
+    <!-- TodoList -->
+    <TodoList
+      v-bind:todoItems="todoItems"
+      v-on:doneToggle="doneToggle"
+      v-on:removeTodo="removeTodo"
+    ></TodoList>
+
+    <!-- TodoFooter -->
+    <TodoFooter v-on:clearAll="clearAll"></TodoFooter>
+  </div>
 </template>
 
 <script>
@@ -29,29 +40,33 @@ import TodoHeader from "@/components/todo/TodoHeader.vue";
 import TodoInput from "@/components/todo/TodoInput.vue";
 import TodoList from "@/components/todo/TodoList.vue";
 import TodoFooter from "@/components/todo/TodoFooter.vue";
-
 export default {
-    /* pdtmc^2w */
-    data: function(){
-        return {
-            todoItems: [],
-        }
-    },
-    methods: {
-        clearAll: function() {
-        },
-        addTodo: function(todoItem) {
-        },
-        removeTodo: function(todoItem, index) {
-        }
-    },
-    created() {
-    },
-    components: {
-        TodoHeader: TodoHeader,
-        TodoInput: TodoInput,
-        TodoList: TodoList,
-        TodoFooter: TodoFooter
-    }
+  /* pdtmc^2w */
+  props: [],
+  data: function () {
+    return {
+      todoItems: [
+        { id: 1, todo: "영화보기", done: false },
+        { id: 2, todo: "주말 산책", done: true },
+        { id: 3, todo: "ES6 학습", done: false },
+        { id: 4, todo: "잠실 야구장", done: false },
+      ],
+    };
+  },
+  //template: ``,
+  methods: {
+    addTodo: function (newTodoItem) {},
+    doneToggle: function (id, index) {},
+    removeTodo: function (id, index) {},
+    clearAll: function () {},
+  },
+  components: {
+    TodoHeader: TodoHeader,
+    TodoInput: TodoInput,
+    TodoList: TodoList,
+    TodoFooter: TodoFooter,
+  },
+  computed: {},
+  watch: {},
 };
 </script>
